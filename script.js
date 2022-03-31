436ec0bc1459c128fc4cc18e1c11314cc672a86a
Samuel Mandel <slman.760@gmail.com>
2022-03-30 20:20:04 -0700

    final rockpaperscissors

--- script.js
+++ script.js
@@ -8,42 +8,68 @@
 //              - 0 = R, 1 = P, 2 = S 
 //          - playerLetter = prompt() 
 // 
-var scoreboard = {
-    scorePlayer: 0,
-    scoreBot: 0,
-};
-//mayber name this pick letter
-function playGame () {
-    let message = window.prompt("Please enter either r, p, or s", "");
-    if (message === 'r') {
-        confirm('Game on')
-        return 'r'
-        
+var wins = 0;
+var ties = 0;
+var losses = 0;
+
+var playGame = function() {
+    var playerLetter = pickLetter();
+    var botLetter = getbotLetter();
+    checkWinner(playerLetter, botLetter);
+    alertStats();
+    
+}
+function pickLetter () {
+    var playerLetter = window.prompt("Please enter either r, p, or s");
+    playerLetter = playerLetter.toUpperCase();
+    if (playerLetter === 'R') {
+        return playerLetter 
     }
-    else if (message === 'p') {
-        confirm('Game on')
-        return 'p'
+    else if (playerLetter === 'P') {
+        return playerLetter
     }
-    else if (message === 's') {
-        confirm('Game on')
-        return 's'
+    else if (playerLetter === 'S') {
+        return playerLetter
     }
     else{
         confirm('Please use r, p or s')
     }
   }
-//starts game and enters choice to console.log 
-var playerLetter = playGame()
-console.log(playerLetter);
-
 //random number between 0 and 2 
-function botLetter() {
-    var RPS = 'rps'
-    return RPS[Math.floor(Math.random()*RPS.length)] 
+function getbotLetter() {
+    var letters = ['R','P','S']
+    var choice = Math.floor(Math.random()*letters.length)
+    botLetter = letters[choice] 
+    return botLetter
     
 }
-console.log(botLetter())
+//checks winner or loser
+function checkWinner (playerLetter, botLetter) {
+    if (playerLetter === botLetter) {
+        ties++;
+        window.alert("It's a tie!");
+    } else if (
+        (playerLetter === "R" && botLetter === "S") || 
+        (playerLetter === "P" && botLetter === "R") || 
+        (playerLetter === "S" && botLetter === "P"))
+        {
+            wins++;
+            window.alert("You win!");
+    } else {
+        losses++;
+        window.alert("You lost!")
+    }
+    // playAgain();
+    // alertStats();
+}
+function alertStats() {
+    window.alert("Stats:\nWins: "+wins +"\nLosses: " + losses +"\nTies: "+ ties);
+    if (confirm("play again?") == true) {
+        playGame();
+    }
+}
 
+playGame();
 
 
 
